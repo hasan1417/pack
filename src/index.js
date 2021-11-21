7d56745b2ef80d72fc90304e24b844051ac1af88
hassan <your_name@domain.com>
2021-11-21 13:56:40 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,51 @@
+import style from './main.css'
+import web3 from 'web3'
+import {Transaction} from '@ethereumjs/tx'
+
+var gateway = new web3('https://rinkeby.infura.io/v3/0f3d9f30356e48c7b048c0b6a6c8ceae')
+var abi = [
+    {
+      "inputs": [],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "inputs": [],
+      "name": "starName",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    },
+    {
+      "inputs": [],
+      "name": "starOwner",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    },
+    {
+      "inputs": [],
+      "name": "claimStar",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    }
+  ]
+
+var con = new gateway.eth.Contract(abi,'0x444C514e0AE9dA12Db399A2d30124ADbB96f8EC4')
+
+const privateKey
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
+const HtmlWebackPlugin = require('html-webpack-plugin');
+module.exports = {
+    mode: 'development',
+    entry: {
+        main: path.resolve(__dirname, 'src/index.js'),
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[contenthash].js',
+        assetModuleFilename: '[name][ext]',
+        clean: true
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: { directory: path.resolve(__dirname, 'dist') },
+        port: 8080,
+        open: true,
+        hot: true,
+        liveReload: true,
+        historyApiFallback: true
+
+    },
+    resolve: {
+        fallback: {
+            "http": require.resolve("stream-http")
+        }
+    },
+    module: {
+        rules: [
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+            { test: /\.(svg|png|ico|webp|jpg|gif|jpeg)$/, type: 'asset/resource' },
+            {
+                test: /\.js$/, exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebackPlugin({
+            title: 'webpack-dev-server',
+            filename: 'index.html',
+            template: path.resolve(__dirname, 'src/temp.html')
+        }),
+        new NodePolyfillPlugin()
+    ]
+}
